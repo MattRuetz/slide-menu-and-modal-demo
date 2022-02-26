4d51fb3ce8ca7fa2ae720f8e76f3092e88844b5b
MattRuetz <matt.b.ruetz@gmail.com>
2022-02-26 17:24:41 -0500

    minor refactor

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const modal = document.getElementById('modal');
 open.addEventListener('click', () => modal.classList.add('show-modal'));
 // Hide Modal on X button
 close.addEventListener('click', () => modal.classList.remove('show-modal'));
-// Hide Modal on click-outside
+// Hide Modal on click-outside-box
 window.addEventListener('click', (e) =>
     e.target == modal ? modal.classList.remove('show-modal') : false
 );
